821540aaa2f96b2c6a729ff38a9164c1539e03e0
tauraamui <adampstringer@protonmail.com>
2023-01-20 23:52:00 +0000

    increase test smtp server r/w deadlines to 1 minute

--- cmd/smtpsvr/main.go
+++ cmd/smtpsvr/main.go
@@ -15,8 +15,8 @@ func main() {
 
 	s.Addr = ":1025"
 	s.Domain = "localhost"
-	s.ReadTimeout = 10 * time.Second
-	s.WriteTimeout = 10 * time.Second
+	s.ReadTimeout = 60 * time.Second
+	s.WriteTimeout = 60 * time.Second
 	s.MaxMessageBytes = 1024 * 1024
 	s.MaxRecipients = 50
 	s.AllowInsecureAuth = true
