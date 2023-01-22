927ad08171cd6dc23c13df50067f132b0af9aec7
tauraamui <adampstringer@protonmail.com>
2023-01-22 00:22:11 +0000

    define client interface along with the test

--- internal/mail/client.go
+++ internal/mail/client.go
@@ -2,16 +2,42 @@ package mail
 
 // TODO: implement client, decide on the interface etc.,
 
+import (
+	"github.com/emersion/go-imap"
+	imapclient "github.com/emersion/go-imap/client"
+)
+
+type Mailbox struct {
+	Name string
+}
+
 type Client interface {
-	Mailboxes() interface{}
+	Mailboxes() ([]Mailbox, error)
 }
 
-func Connect(email, password string) Client {
-	return client{}
+func Connect(email, password string) (Client, error) {
+	c, err := imapclient.Dial(":1143")
+	return client{
+		client: c,
+	}, err
 }
 
-type client struct{}
+type client struct {
+	client *imapclient.Client
+}
 
-func (c client) Mailboxes() interface{} {
-	return nil
+func (c client) Mailboxes() ([]Mailbox, error) {
+	mailboxesChan := make(chan *imap.MailboxInfo, 10)
+	done := make(chan error, 1)
+
+	mailboxes := []Mailbox{}
+	go func() {
+		done <- c.client.List("", "", mailboxesChan)
+	}()
+
+	for m := range mailboxesChan {
+		mailboxes = append(mailboxes, Mailbox{m.Name})
+	}
+
+	return mailboxes, <-done
 }
--- internal/mail/client_test.go
+++ internal/mail/client_test.go
@@ -3,6 +3,8 @@ package mail_test
 import (
 	"testing"
 
+	"github.com/emersion/go-imap/backend/memory"
+	"github.com/emersion/go-imap/server"
 	"github.com/matryer/is"
 	"github.com/tauraamui/maildew/internal/mail"
 )
@@ -10,7 +12,28 @@ import (
 func TestClientListingMailboxes(t *testing.T) {
 	is := is.New(t)
 
-	client := mail.Connect("username", "password")
+	errc := make(chan error, 1)
+	shutdown := startLocalServer(errc)
+	defer shutdown()
 
-	is.Equal(client.Mailboxes(), nil)
+	// FIX: connection refused not sure why
+	client, err := mail.Connect("username", "password")
+	is.NoErr(err)
+
+	mailboxes, err := client.Mailboxes()
+	is.NoErr(err)
+	is.Equal(mailboxes, nil)
+}
+
+func startLocalServer(errc chan error) func() error {
+	s := server.New(memory.New())
+
+	s.Addr = ":1143"
+	s.AllowInsecureAuth = true
+
+	go func(errc chan error) {
+		errc <- s.ListenAndServe()
+	}(errc)
+
+	return s.Close
 }
