0757120a3f2eb2fd4b27f42740652edcccd04378
tauraamui <adampstringer@protonmail.com>
2023-05-08 22:43:05 +0100

    feat: define new component base for mailbox listing

---
+++ pkg/tui/mailbox_list.go
@@ -0,0 +1,41 @@
+package tui
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/tauraamui/maildew/pkg/logging"
+	"github.com/tauraamui/maildew/pkg/mail"
+)
+
+type mailboxListModel struct {
+	log        logging.I
+	windowSize tea.WindowSizeMsg
+	mbrepo     mail.MailboxRepo
+}
+
+func initialMailboxListModel(log logging.I, mbrepo mail.MailboxRepo) mailboxListModel {
+	return mailboxListModel{
+		log:    log,
+		mbrepo: mbrepo,
+	}
+}
+
+func (m mailboxListModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m mailboxListModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.windowSize = msg
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "esc":
+			return m, tea.Quit
+		}
+	}
+	return nil, nil
+}
+
+func (m mailboxListModel) View() string {
+	return "mailbox list"
+}
