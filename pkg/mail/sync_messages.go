0ec764b5df3cd423afd46ab9b9b8e0300faf5716
tauraamui <adampstringer@protonmail.com>
2023-07-03 14:09:07 +0100

    feat: implement error aware reading of mailbox messages from connection

--- pkg/mail/sync_messages.go
+++ pkg/mail/sync_messages.go
@@ -1,6 +1,9 @@
 package mail
 
-import "github.com/tauraamui/maildew/pkg/logging"
+import (
+	"github.com/emersion/go-imap"
+	"github.com/tauraamui/maildew/pkg/logging"
+)
 
 func SyncMessages(
 	log logging.I,
@@ -12,5 +15,52 @@ func SyncMessages(
 }
 
 func syncMailboxMessages(conn RemoteConnection, msgr MessageRepo, mb Mailbox) error {
+	if err := forEachMessage(conn, mb.Name, func(name string) error {
+		if err := msgr.Save(mb.UUID, Message{}); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
 	return nil
+}
+
+func forEachMessage(conn RemoteConnection, mailboxName string, callback func(name string) error) error {
+	mb, err := conn.Select(mailboxName, true)
+	if err != nil {
+		return err
+	}
+
+	msgsc := make(chan *imap.Message, 1)
+	errc := make(chan error, 1)
+	// NOTE:(tauraamui) the implementation of fetch already closes our receiver channel on completion
+	go func() {
+		errc <- conn.Fetch(buildSequence(mb.Messages), []imap.FetchItem{imap.FetchEnvelope}, msgsc)
+	}()
+
+	for {
+		select {
+		case err := <-errc:
+			if err != nil {
+				return err
+			}
+		case msg, more := <-msgsc:
+			if err := callback(msg.Envelope.Subject); err != nil {
+				return err
+			}
+
+			if !more {
+				return nil
+			}
+		}
+	}
+}
+
+func buildSequence(msgs uint32) *imap.SeqSet {
+	from := uint32(1)
+	to := msgs
+	seqset := imap.SeqSet{}
+	seqset.AddRange(from, to)
+	return &seqset
 }
